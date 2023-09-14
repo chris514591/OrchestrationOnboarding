42b327bf58acb9597753fe119b257012008c9135
Chris de Bruijn <xevichris@gmail.com>
2023-09-14 14:07:15 +0200

    first commit

---
+++ OnboardingCreateUser.ps1
@@ -0,0 +1,47 @@
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Define the path to the CSV file containing user information
+$csvPath = "S:\Fileshare\HR\NewHires.csv"
+
+# Define the target OU where you want to create users
+$ouPath = "OU=TestOU,DC=CDB,DC=lan"
+
+# Check if the CSV file exists
+if (Test-Path $csvPath) {
+    # Read the CSV file
+    $userList = Import-Csv $csvPath
+
+    # Loop through each user in the CSV
+    foreach ($user in $userList) {
+        $firstName = $user.Firstname
+        $lastName = $user.Surname
+        $function = $user.Function
+        $department = $user.Department
+        $location = $user.Location
+        $password = $user.Password  # Password from the CSV
+
+        # Check if the user already exists
+        $existingUser = Get-ADUser -Filter { (GivenName -eq $firstName) -and (Surname -eq $lastName) } -ErrorAction SilentlyContinue
+
+        if ($existingUser -eq $null) {
+            # Create a new user in Active Directory in the specified OU
+            try {
+                New-ADUser -Name "$firstName $lastName" -GivenName $firstName -Surname $lastName -UserPrincipalName "$firstName.$lastName@CDB.lan" -SamAccountName $firstName -Enabled $true -Path $ouPath -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -ErrorAction Stop
+
+                # Set additional user attributes
+                Set-ADUser -Identity "$firstName $lastName" -Description $function -Department $department -Office $location -ErrorAction Stop
+
+                Write-Host "User '$firstName $lastName' created in $ouPath with the specified password."
+            } catch {
+                Write-Host "Error creating user '$firstName $lastName': $_"
+            }
+        } else {
+            Write-Host "User '$firstName $lastName' already exists. Skipping creation."
+        }
+    }
+
+    Write-Host "User creation/update completed."
+} else {
+    Write-Host "CSV file not found at $csvPath"
+}
